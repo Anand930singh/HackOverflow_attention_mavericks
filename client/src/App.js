079656eb52f23c5a6259c02bb5ebc9a0aac6d669
Anish-kumar7641 <72142986+Anish-kumar7641@users.noreply.github.com>
2024-03-14 23:37:32 +0530

    added signup and signin

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
 import Home from "./pages/Home/Home";
+import LoginSignup from './components/Authentication/LoginSignup'
+import Navbar from './components/Navbar/Navbar'
+import Project from './components/Project/Project';
+import Comments from './components/Comments/Comments';
 
 function App() {
   return (
-    <BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route exact path="/home" element={<Home />} />
+        <Route path='/loginSignup' element={<LoginSignup/>}/>
+      <Route path='/project' element={<Project/>}/>
+      <Route path='/comment' element={<Comments/>}/>
       </Routes>
     </BrowserRouter>
+    </div>
+    
   );
 }
 
---
+++ client/src/components/Authentication/AdminLogin.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import AdminSignup from './AdminSignup';
+
+function AdminLogin() {
+  const [showLogin, setShowLogin] = useState(true);
+  const [showSignup, setShowSignup] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleShowSignup = () => {
+    setShowSignup(true); 
+    setShowLogin(false); 
+  };
+
+  return (
+    <div className="form-container">
+      {showLogin && (
+        <>
+      <h2>Admin Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      <h4>Don't have an account? <button onClick={handleShowSignup}>Signup Here</button></h4>
+      </>
+      )} 
+      {showSignup && <AdminSignup />}
+    </div>
+  );
+}
+
+export default AdminLogin;
